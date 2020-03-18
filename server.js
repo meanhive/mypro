f1d7e2514336e61512e647d8a1341aba10e87618
MEAN Hive <meanhive@zoho.com>
2020-03-18 16:59:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,10 +2,18 @@ const express = require('express');
 const app = express();
 const PORT = Number(process.env.PORT || 3400);
 
-app.use('/', (req, res) => {
+
+app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
+app.get('/about', (req, res) => {
+  res.sendFile(__dirname + '/about.html');
+});
+
+app.get('/contact', (req, res) => {
+  res.sendFile(__dirname + '/contact.html');
+});
 app.listen(PORT, () => {
   console.log(`server is running in http://localhost:3400`);
 });
